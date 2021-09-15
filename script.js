907653a51d193f347b5a44bcb6fc0fcdb78aa29e
Hosamov <hosamov@hotmail.com>
2021-09-15 10:45:04 -0700

    add pseudocode for handling War, add new tieHandler() function

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 import devices from './gameLogic.js';
+
+//DOM Elements:
 const container = document.getElementById('container');
 const playArea = document.getElementById('play-area');
-const aiPlayArea = document.querySelector('.ai-play-area');
-const playerPlayArea = document.querySelector('.player-play-area');
 const uiPlayer = document.querySelector('.ui-player');
 const uiComputer = document.querySelector('.ui-computer');
-const playerCard = document.querySelector('.player-card');
 
+//Declare global variables:
 const newDeck = []; //init newDeck var to hold all card values before dispersing
 const playerDecks =[[],[]];
+const tieArr = []; // Array to keep track of tied cards
 const cardImageBack = './images/card_back.png';
-
-let cardFlipped = false; // Globally track whether card is flipped
 let [p1, p2] = playerDecks; //p1 = human; p2 = ai
+let cardFlipped = false; // Globally track whether card is flipped
 
 /*
 * Create a new deck with 32 cards (Game start):
@@ -54,7 +54,7 @@ function uiHandler() {
   uiComputer.innerHTML += " AI: " + p2.length + " cards";
 }
 
-function addCard(p1ImageFront, p2ImageFront) { //classes: ai-card, player-card
+function addUpdateCard(p1ImageFront, p2ImageFront) { //classes: ai-card, player-card
   playArea.innerHTML = ''; //First, clear the play area
   (!cardFlipped) ?
   playArea.insertAdjacentHTML('beforeend', `
@@ -82,13 +82,15 @@ function addCard(p1ImageFront, p2ImageFront) { //classes: ai-card, player-card
 */
 function runGameInstance(p1Card, p2Card) {
   // Add starting cards
-  addCard(cardImageHandler(p1Card), cardImageHandler(p2Card));
+  addUpdateCard(cardImageHandler(p1Card), cardImageHandler(p2Card));
+  const playerCard = document.querySelector('.player-card img');
 
-  // Test logic once game area has been clicked on by player:
-  playArea.addEventListener('click', () => {
+  // Perform logic once game area has been clicked on by player:
+  playerCard.addEventListener('click', () => {
     cardFlipped = true;
-    addCard(cardImageHandler(p1Card), cardImageHandler(p2Card)); //flip cards over
+    addUpdateCard(cardImageHandler(p1Card), cardImageHandler(p2Card)); //flip cards over
 
+    // Loop through devices array to check cards against each other:
     devices.forEach(device => {
       if(p1Card === device.device) {
         console.log(p1);
@@ -96,15 +98,25 @@ function runGameInstance(p1Card, p2Card) {
         console.log('You drew ' + device.device);
         if(p2Card === device.win) {
           console.log('You win! ' + device.device + ' beats ' + device.win);
+          tieHandler(p1);
           gameUpdateHandler(p1, p2, device.device, device.device);
         } else if(p2Card === device.lose) {
           console.log('You lost... ' + device.device + ' loses to ' + device.lose);
+          tieHandler(p2);
           gameUpdateHandler(p2, p1, device.device, device.device);
-        } else {
-          console.log('Tie round.');
+        } else { // Begin War...
           //TODO: Write code for tie state...
             // Continue drawing until one player draws the higher card
-            // Push all cards to the winning player's array
+            //  If two of the same card exist,
+              //  remove them both from the respective players
+              //  add them both to tieArr for future use
+            //  When a player wins the round,
+              // Push all cards to the winning player's array
+
+          tieArr.push(p1Card, p2Card);
+          console.log('Tie round.');
+          console.log(tieArr);
+
         }
       }
     });
@@ -122,6 +134,13 @@ function gameUpdateHandler(arr1, arr2, losingCard, winningCard) {
   console.log(p1, p2);
 }
 
+function tieHandler(playerArr) {
+  if(tieArr.length) {
+    playerArr.push(...tieArr);
+    tieArr = [];
+  }
+}
+
 /*
 * Get and return the corresponding device image randomly from assets list:
 */
@@ -136,9 +155,9 @@ function cardImageHandler(card) {
 
 // TODO: Game logic:
 //If player's card beats opponent's, player takes both cards (check)
-// otherwise, if opponent's card wins, opponent takes both cards. (check)
+  // otherwise, if opponent's card wins, opponent takes both cards. (check)
 //If both cards are the same, WAR begins:
-//play until a user wins... winner takes all cards currently in the play area.
+  //play until a user wins... winner takes all cards currently in the play area.
 
 //TODO: Add click handler for player to start a new game (shuffle deck)
 //TODO: Add ability for Player to add their name as the current player
